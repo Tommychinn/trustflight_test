5834c379147b5bf8609e09fed749405e25b48eb7
Tommmychinn <tommychinn1@gmail.com>
2020-07-09 08:41:04 +0100

    adding initial tasks to map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            task: "change engine 4",
+        },
+        {
+            task: "replace windshield",
+        },
+        {
+            task: "engine service",
+        },
+        {
+            task: "replace tyres",
+        },
+    ]);
     return (
         <div className="App">
-            <h1>hello</h1>
+            {tasks.map((task) => (
+                <div>{task.task}</div>
+            ))}
         </div>
     );
 }
